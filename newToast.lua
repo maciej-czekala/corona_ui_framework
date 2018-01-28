6318deaa245918338d3d1514868709f653fc6581
mczekala-insys <m.czekala@insys.pl>
2018-01-28 14:04:56 +0100

    added new toast info

--- newToast.lua
+++ newToast.lua
@@ -6,5 +6,7 @@ ui:init()
 
 
 --ui.newToast("my toast!", 5000) -- (string, hide delay) 
-ui.newToast("my toast!") -- pass string to be displayed
+ui.newToast({label = {text = "my toast!"}}) -- pass string to be displayed
+
+-- ui.newToast({config = {color = {1,1,1}, x = 100, y = 100} label = {text = "my toast!", color = {1,0,0} } })
 
---
+++ newToastLegacy.lua
@@ -0,0 +1,10 @@
+-- Examples
+
+
+local ui = require("ui_framework.ui_framework")
+ui:init()
+
+
+--ui.newToast("my toast!", 5000) -- (string, hide delay) 
+ui.newToast("my toast!") -- pass string to be displayed
+
